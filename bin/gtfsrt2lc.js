cadf678a8ebf0f70f1b191c6c6eeadff6cc0de22
Haroen Viaene <hello@haroen.me>
2016-07-23 17:22:49 +0200

    fix some style errors
    following `xo` as a lead. Errors still present are:
    
    ```
      bin/gtfsrt2lc.js:3:1
      ✖    3:1   Do not mix core, module, file and computed requires.  no-mixed-requires
      ✖    3:1   Split var declarations into multiple statements.      one-var
      ✖   25:7   output is defined but never used                      no-unused-vars
      ✖   35:7   mongoId is defined but never used                     no-unused-vars
      ✖   40:11  Identifier trip_update is not in camel case.          camelcase
      ✖   41:11  Identifier trip_id is not in camel case.              camelcase
      ✖   41:21  Identifier trip_update is not in camel case.          camelcase
      ✖   42:11  Identifier gtfs_route is not in camel case.           camelcase
      ✖   50:61  Identifier stop_time is not in camel case.            camelcase
      ✖   51:13  Identifier stop_times_length is not in camel case.    camelcase
      ✖   51:33  Identifier trip_update is not in camel case.          camelcase
      ✖   52:29  Identifier stop_time is not in camel case.            camelcase
      ✖   54:23  Expected === and instead saw ==.                      eqeqeq
      ✖   54:26  Identifier stop_times_length is not in camel case.    camelcase
      ✖   55:15  arrivalStop used outside of binding context.          block-scoped-var
      ✖   57:15  arrivalStop used outside of binding context.          block-scoped-var
      ✖   57:15  arrivalStop is already defined                        no-redeclare
      ✖   59:9   Split var declarations into multiple statements.      one-var
      ✖   62:13  Identifier stop_time is not in camel case.            camelcase
      ✖   62:34  Identifier stop_time is not in camel case.            camelcase
      ✖   62:60  Identifier stop_time is not in camel case.            camelcase
      ✖   63:34  Identifier stop_time is not in camel case.            camelcase
      ✖   65:14  Identifier stop_time is not in camel case.            camelcase
      ✖   65:38  Identifier stop_time is not in camel case.            camelcase
      ✖   65:67  Identifier stop_time is not in camel case.            camelcase
      ✖   66:11  Unexpected negated condition.                         no-negated-condition
      ✖   72:36  Identifier stop_time is not in camel case.            camelcase
      ✖   78:35  Identifier stop_time is not in camel case.            camelcase
      ✖   78:55  Identifier stop_time is not in camel case.            camelcase
      ✖   79:37  Identifier stop_time is not in camel case.            camelcase
      ✖   79:59  Identifier stop_time is not in camel case.            camelcase
      ✖   88:63  arrivalStop used outside of binding context.          block-scoped-var
      ✖   88:63  arrivalStop used outside of binding context.          block-scoped-var
      ✖   93:51  Identifier trip_id is not in camel case.              camelcase
      ✖   94:59  Identifier gtfs_route is not in camel case.           camelcase
      ✖  117:10  Unexpected else after return.                         no-else-return
      ✖  126:28  Expected === and instead saw ==.                      eqeqeq
      ✖  128:35  Expected === and instead saw ==.                      eqeqeq
      ✖  131:7   responseBody is defined but never used                no-unused-vars
      ✖  134:5   Unexpected negated condition.                         no-negated-condition
    
      40 errors
    ```

--- bin/gtfsrt2lc.js
+++ bin/gtfsrt2lc.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 
-var program = require('commander'),
-    http = require('http'),
-    zlib = require('zlib'),
-    gtfsrt = require('gtfs-realtime-bindings'),
-    url = require('url'),
-    fs = require('fs');
+var http = require('http'),
+  zlib = require('zlib'),
+  url = require('url'),
+  gtfsrt = require('gtfs-realtime-bindings'),
+  program = require('commander');
 
-
-console.error("GTFS-RT of iRail to linked connections converter use --help to discover more functions");
+console.error('GTFS-RT of iRail to linked connections converter use --help to discover more functions');
 
 program
   .arguments('<url>', 'URL to gtfs-rt')
@@ -22,61 +20,51 @@ if (!program.url) {
   process.exit();
 }
 
-//When we have a response, parse the gtfsrt feed and create connections from them
+// When we have a response, parse the gtfsrt feed and create connections from them
 var onResponse = function (error, response, body) {
-
   var output = [];
 
   /**
-  * Base URI of iRail connections
-  * @var iRailConnectionUrl
-  */
-  var iRailConnectionUrl = "http://irail.be/connections/";
+   * Base URI of iRail connections
+   * @var iRailConnectionUrl
+   */
+  var iRailConnectionUrl = 'http://irail.be/connections/';
 
   var feed = gtfsrt.FeedMessage.decode(body);
-  
+
   var mongoId = iRailConnectionUrl;
 
-  feed.entity.forEach(function(entity) {
-
+  feed.entity.forEach(function (entity) {
     // does the entity have updates?
-    if (entity.trip_update)
-    {
-
+    if (entity.trip_update) {
       var trip_update = entity.trip_update;
       var trip_id = trip_update.trip.trip_id;
       var gtfs_route = entity.trip_update.vehicle.id;
 
       /**
-      * Check if train is canceled or not
-      */
+       * Check if train is canceled or not
+       */
       var type = getConnectionType(entity);
-      
 
       // foreach stop time update
-      entity.trip_update.stop_time_update.forEach(function (stop_time, index)
-      {
-
+      entity.trip_update.stop_time_update.forEach(function (stop_time, index) {
         var stop_times_length = trip_update.stop_time_update.length;
         var departureStop = stop_time.stop_id.split(':')[0];
 
-        if(index+1 == stop_times_length)
-        {
+        if (index + 1 == stop_times_length) {
           var arrivalStop = departureStop;
-        }
-        else
-        {
-          var arrivalStop   = entity.trip_update.stop_time_update[index+1].stop_id.split(':')[0];
+        } else {
+          var arrivalStop = entity.trip_update.stop_time_update[index + 1].stop_id.split(':')[0];
         }
         var arrivalTime = null,
-            departureTime = null;
-        //Check whether arrival time and/or departure time is set
+          departureTime = null;
+        // Check whether arrival time and/or departure time is set
         if (stop_time.arrival && stop_time.arrival.time && stop_time.arrival.time.low) {
           arrivalTime = new Date(stop_time.arrival.time.low * 1000);
         }
         if (!stop_time.departure || !stop_time.departure.time || !stop_time.departure.time.low) {
           if (!arrivalTime) {
-            //do nothing: both arrival as departure is not set: this stoptime is skipped?
+            // do nothing: both arrival as departure is not set: this stoptime is skipped?
           } else {
             departureTime = arrivalTime;
           }
@@ -87,70 +75,58 @@ var onResponse = function (error, response, body) {
           }
         }
 
-        
-        var arrivalDelaySeconds  = stop_time.arrival? stop_time.arrival.delay : 0;
-        var departureDelaySeconds  = stop_time.departure? stop_time.departure.delay : arrivalDelaySeconds;
-        var mongoId = 'http://irail.be/connections/' 
-                      + encodeURIComponent(departureStop)
-                      + 
-                      '/'
-                      +
-                      encodeURIComponent(departureTime.toISOString().substr(0,10))
-                      + '/'
-                      + encodeURIComponent(gtfs_route); 
+        var arrivalDelaySeconds = stop_time.arrival ? stop_time.arrival.delay : 0;
+        var departureDelaySeconds = stop_time.departure ? stop_time.departure.delay : arrivalDelaySeconds;
+        var mongoId = 'http://irail.be/connections/' + encodeURIComponent(departureStop) +
+          '/' +
+          encodeURIComponent(departureTime.toISOString().substr(0, 10)) + '/' + encodeURIComponent(gtfs_route);
 
         var obj = {
-          "@id"             : mongoId,
-          "@type"           : type,
-          "departureStop"   : "http://irail.be/stations/NMBS/00" + departureStop,
-          "arrivalStop"     : "http://irail.be/stations/NMBS/00" + arrivalStop,
-          "arrivalTime"     : arrivalTime.toISOString(),
-          "departureTime"   : departureTime.toISOString(),
-          "arrivalDelay"    : arrivalDelaySeconds,
-          "departureDelay"  : departureDelaySeconds,
-          "gtfs:trip"       : "http://irail.be/trips/" + trip_id,
-          "gtfs:route"      : "https://irail.be/vehicle/?id=" + gtfs_route
-        }
+          '@id': mongoId,
+          '@type': type,
+          'departureStop': 'http://irail.be/stations/NMBS/00' + departureStop,
+          'arrivalStop': 'http://irail.be/stations/NMBS/00' + arrivalStop,
+          'arrivalTime': arrivalTime.toISOString(),
+          'departureTime': departureTime.toISOString(),
+          'arrivalDelay': arrivalDelaySeconds,
+          'departureDelay': departureDelaySeconds,
+          'gtfs:trip': 'http://irail.be/trips/' + trip_id,
+          'gtfs:route': 'https://irail.be/vehicle/?id=' + gtfs_route
+        };
 
         // print object
         console.log(JSON.stringify(obj));
       });
       // end foreach
 
-      //reset mongoId
+      // reset mongoId
       mongoId = iRailConnectionUrl;
     } // end if
   });
 };
 
-
 /**
-* Get connection type
-* @author Serkan Yildiz
-* @param Object entity
-* @return string Connection|CanceledConnection
-*/
-function getConnectionType(entity)
-{
-  if (entity.is_deleted)
-  {
+ * Get connection type
+ * @author Serkan Yildiz
+ * @param Object entity
+ * @return string Connection|CanceledConnection
+ */
+function getConnectionType(entity) {
+  if (entity.is_deleted) {
     return 'CanceledConnection';
-  }
-  else
-  {
+  } else {
     return 'Connection';
   }
 }
 
-
-//Step 1: fetch the GTFS
+// Step 1: fetch the GTFS
 http.request(program.url, function (res) {
-  var encoding = res.headers['content-encoding']
+  var encoding = res.headers['content-encoding'];
   var responseStream = res;
   if (encoding && encoding == 'gzip') {
     responseStream = res.pipe(zlib.createGunzip());
   } else if (encoding && encoding == 'deflate') {
-    responseStream = res.pipe(zlib.createInflate())
+    responseStream = res.pipe(zlib.createInflate());
   }
   var responseBody = '';
   var buffer = false;
@@ -158,7 +134,7 @@ http.request(program.url, function (res) {
     if (!buffer) {
       buffer = chunk;
     } else {
-      buffer = Buffer.concat([buffer, chunk], buffer.length+chunk.length);
+      buffer = Buffer.concat([buffer, chunk], buffer.length + chunk.length);
     }
   });
   res.on('error', function (error) {
@@ -166,6 +142,5 @@ http.request(program.url, function (res) {
   });
   responseStream.on('end', function () {
     onResponse(null, res, buffer);
-  })
+  });
 }).end();
-
